2b6971815f6449a98465e7beaff3c6315743125c
devrai <devraimailbox@gmail.com>
2020-09-08 18:52:12 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDT6tYwY19lOujhEYXYeR_UNc57Eh64yqA",
+    authDomain: "netflix-clone-1f077.firebaseapp.com",
+    databaseURL: "https://netflix-clone-1f077.firebaseio.com",
+    projectId: "netflix-clone-1f077",
+    storageBucket: "netflix-clone-1f077.appspot.com",
+    messagingSenderId: "77432952646",
+    appId: "1:77432952646:web:9993fc1a9cafc24dcb7747"
+};
---
+++ src/Nav.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import './Nav.css';
+
+function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+
+            if (window.scrollY > 270) {
+                handleShow(true);
+            }
+            else {
+                handleShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+
+    }, []);
+
+
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <img
+                className="nav__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png"
+                alt="Netflix logo"
+            />
+
+
+            <img
+                className="nav__avatar"
+                src="https://i.pinimg.com/564x/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.jpg"
+                alt="Netflix logo"
+            />
+
+
+        </div>
+    )
+}
+
+export default Nav
